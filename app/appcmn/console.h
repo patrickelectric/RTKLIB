0d0f23802d49a831bae167082891a4273c747e44
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:43 -0300

    console.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/appcmn/console.h
+++ app/appcmn/console.h
@@ -13,31 +13,31 @@
 class TConsole : public TForm
 {
 __published:
-	TPanel *Panel1;
-	TPanel *Panel2;
-	TButton *BtnClose;
-	TSpeedButton *BtnAsc;
-	TSpeedButton *BtnHex;
-	TSpeedButton *BtnClear;
-	TPaintBox *Console;
-	TScrollBar *Scroll;
-	TSpeedButton *BtnDown;
-	TSpeedButton *BtnStop;
-	void __fastcall BtnCloseClick(TObject *Sender);
-	void __fastcall BtnClearClick(TObject *Sender);
-	void __fastcall ConsolePaint(TObject *Sender);
-	void __fastcall ScrollChange(TObject *Sender);
-	void __fastcall FormResize(TObject *Sender);
-	void __fastcall BtnAscClick(TObject *Sender);
-	void __fastcall BtnHexClick(TObject *Sender);
-	void __fastcall BtnDownClick(TObject *Sender);
-	void __fastcall BtnStopClick(TObject *Sender);
+    TPanel *Panel1;
+    TPanel *Panel2;
+    TButton *BtnClose;
+    TSpeedButton *BtnAsc;
+    TSpeedButton *BtnHex;
+    TSpeedButton *BtnClear;
+    TPaintBox *Console;
+    TScrollBar *Scroll;
+    TSpeedButton *BtnDown;
+    TSpeedButton *BtnStop;
+    void __fastcall BtnCloseClick(TObject *Sender);
+    void __fastcall BtnClearClick(TObject *Sender);
+    void __fastcall ConsolePaint(TObject *Sender);
+    void __fastcall ScrollChange(TObject *Sender);
+    void __fastcall FormResize(TObject *Sender);
+    void __fastcall BtnAscClick(TObject *Sender);
+    void __fastcall BtnHexClick(TObject *Sender);
+    void __fastcall BtnDownClick(TObject *Sender);
+    void __fastcall BtnStopClick(TObject *Sender);
 private:
-	TStringList *ConBuff;
-	int Stop,ScrollPos;
+    TStringList *ConBuff;
+    int Stop,ScrollPos;
 public:
-	__fastcall TConsole(TComponent* Owner);
-	void __fastcall AddMsg(unsigned char *buff, int n);
+    __fastcall TConsole(TComponent* Owner);
+    void __fastcall AddMsg(unsigned char *buff, int n);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TConsole *Console;
