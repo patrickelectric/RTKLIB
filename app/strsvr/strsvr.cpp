1bc9fb4391bd211199906feec825649c111fdf85
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:47 -0300

    strsvr.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/strsvr/strsvr.cpp
+++ app/strsvr/strsvr.cpp
@@ -22,41 +22,33 @@ USEFORM("..\appcmn\vieweropt.cpp", ViewerOptDialog);
 //---------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	try
-	{
-		Application->Initialize();
-		Application->Title = "STRSVR";
-		Application->CreateForm(__classid(TMainForm), &MainForm);
-		Application->CreateForm(__classid(TSvrOptDialog), &SvrOptDialog);
-		Application->CreateForm(__classid(TCmdOptDialog), &CmdOptDialog);
-		Application->CreateForm(__classid(TFileOptDialog), &FileOptDialog);
-		Application->CreateForm(__classid(TSerialOptDialog), &SerialOptDialog);
-		Application->CreateForm(__classid(TFtpOptDialog), &FtpOptDialog);
-		Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
-		Application->CreateForm(__classid(TRefDialog), &RefDialog);
-		Application->CreateForm(__classid(TTcpOptDialog), &TcpOptDialog);
-		Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
-		Application->CreateForm(__classid(TConvDialog), &ConvDialog);
-		Application->CreateForm(__classid(TStrMonDialog), &StrMonDialog);
-		Application->CreateForm(__classid(TTextViewer), &TextViewer);
-		Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
-		Application->Run();
-	}
-	catch (Exception &exception)
-	{
-		Application->ShowException(&exception);
-	}
-	catch (...)
-	{
-		try
-		{
-			throw Exception("");
-		}
-		catch (Exception &exception)
-		{
-			Application->ShowException(&exception);
-		}
-	}
-	return 0;
+    try {
+        Application->Initialize();
+        Application->Title = "STRSVR";
+        Application->CreateForm(__classid(TMainForm), &MainForm);
+        Application->CreateForm(__classid(TSvrOptDialog), &SvrOptDialog);
+        Application->CreateForm(__classid(TCmdOptDialog), &CmdOptDialog);
+        Application->CreateForm(__classid(TFileOptDialog), &FileOptDialog);
+        Application->CreateForm(__classid(TSerialOptDialog), &SerialOptDialog);
+        Application->CreateForm(__classid(TFtpOptDialog), &FtpOptDialog);
+        Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
+        Application->CreateForm(__classid(TRefDialog), &RefDialog);
+        Application->CreateForm(__classid(TTcpOptDialog), &TcpOptDialog);
+        Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
+        Application->CreateForm(__classid(TConvDialog), &ConvDialog);
+        Application->CreateForm(__classid(TStrMonDialog), &StrMonDialog);
+        Application->CreateForm(__classid(TTextViewer), &TextViewer);
+        Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
+        Application->Run();
+    } catch(Exception &exception) {
+        Application->ShowException(&exception);
+    } catch(...) {
+        try {
+            throw Exception("");
+        } catch(Exception &exception) {
+            Application->ShowException(&exception);
+        }
+    }
+    return 0;
 }
 //---------------------------------------------------------------------------
