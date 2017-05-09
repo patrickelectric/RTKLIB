fba037f6c161eb228e659a7e4f25048dbf718105
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:44 -0300

    rtkget.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkget/rtkget.cpp
+++ app/rtkget/rtkget.cpp
@@ -31,34 +31,26 @@ USEFORM("staoptdlg.cpp", StaListDialog);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	try
-	{
-		Application->Initialize();
-		Application->CreateForm(__classid(TMainForm), &MainForm);
-		Application->CreateForm(__classid(TDownOptDialog), &DownOptDialog);
-		Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
-		Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
-		Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
-		Application->CreateForm(__classid(TTextViewer), &TextViewer);
-		Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
-		Application->CreateForm(__classid(TStaListDialog), &StaListDialog);
-		Application->Run();
-	}
-	catch (Exception &exception)
-	{
-		Application->ShowException(&exception);
-	}
-	catch (...)
-	{
-		try
-		{
-			throw Exception("");
-		}
-		catch (Exception &exception)
-		{
-			Application->ShowException(&exception);
-		}
-	}
-	return 0;
+    try {
+        Application->Initialize();
+        Application->CreateForm(__classid(TMainForm), &MainForm);
+        Application->CreateForm(__classid(TDownOptDialog), &DownOptDialog);
+        Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
+        Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
+        Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
+        Application->CreateForm(__classid(TTextViewer), &TextViewer);
+        Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
+        Application->CreateForm(__classid(TStaListDialog), &StaListDialog);
+        Application->Run();
+    } catch(Exception &exception) {
+        Application->ShowException(&exception);
+    } catch(...) {
+        try {
+            throw Exception("");
+        } catch(Exception &exception) {
+            Application->ShowException(&exception);
+        }
+    }
+    return 0;
 }
 //---------------------------------------------------------------------------
