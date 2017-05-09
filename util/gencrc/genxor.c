03e7d10d7f12fcc2eb079ddcdceeb22823c273da
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:49 -0300

    genxor.c: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- util/gencrc/genxor.c
+++ util/gencrc/genxor.c
@@ -6,11 +6,13 @@
 int main(int argc, char **argv)
 {
     int i,j,b;
-    
+
     printf("const unsigned char xor_bits[]={\n");
-    
-    for (i=0;i<256;i++) {
-        for (j=b=0;j<8;j++) b^=((i>>j)&1);
+
+    for(i=0; i<256; i++) {
+        for(j=b=0; j<8; j++) {
+            b^=((i>>j)&1);
+        }
         printf("%d%s",b,i==255?"\n":(i%32==31?",\n":","));
     }
     printf("};\n");
