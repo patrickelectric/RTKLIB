69780d443e466942bb9e34092ab69ca8126e8b6f
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:43 -0300

    tcpoptdlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/appcmn/tcpoptdlg.h
+++ app/appcmn/tcpoptdlg.h
@@ -13,31 +13,31 @@
 class TTcpOptDialog : public TForm
 {
 __published:
-	TButton *BtnCancel;
-	TButton *BtnOk;
-	TEdit *Port;
-	TLabel *LabelAddr;
-	TLabel *LabelPort;
-	TEdit *User;
-	TEdit *Passwd;
-	TLabel *LabelUser;
-	TLabel *LabelPasswd;
-	TLabel *LabelMntPnt;
-	TEdit *Str;
-	TLabel *LabelStr;
-	TComboBox *Addr;
-	TComboBox *MntPnt;
-	TButton *BtnNtrip;
-	void __fastcall FormShow(TObject *Sender);
-	void __fastcall BtnOkClick(TObject *Sender);
-	void __fastcall BtnNtripClick(TObject *Sender);
+    TButton *BtnCancel;
+    TButton *BtnOk;
+    TEdit *Port;
+    TLabel *LabelAddr;
+    TLabel *LabelPort;
+    TEdit *User;
+    TEdit *Passwd;
+    TLabel *LabelUser;
+    TLabel *LabelPasswd;
+    TLabel *LabelMntPnt;
+    TEdit *Str;
+    TLabel *LabelStr;
+    TComboBox *Addr;
+    TComboBox *MntPnt;
+    TButton *BtnNtrip;
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall BtnOkClick(TObject *Sender);
+    void __fastcall BtnNtripClick(TObject *Sender);
 private:
-	void __fastcall AddHist(TComboBox *list, AnsiString *hist);
-	int __fastcall ExecCmd(AnsiString cmd, int show);
+    void __fastcall AddHist(TComboBox *list, AnsiString *hist);
+    int __fastcall ExecCmd(AnsiString cmd, int show);
 public:
-	int Opt;
-	AnsiString Path,History[MAXHIST],MntpHist[MAXHIST];
-	__fastcall TTcpOptDialog(TComponent* Owner);
+    int Opt;
+    AnsiString Path,History[MAXHIST],MntpHist[MAXHIST];
+    __fastcall TTcpOptDialog(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TTcpOptDialog *TcpOptDialog;
