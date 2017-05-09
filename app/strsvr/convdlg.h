51153d58d56d88d2afbd8b5d8dd3a10abfc7a59b
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:47 -0300

    convdlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/strsvr/convdlg.h
+++ app/strsvr/convdlg.h
@@ -12,27 +12,27 @@
 class TConvDialog : public TForm
 {
 __published:
-	TButton *BtnOk;
-	TButton *BtnCancel;
-	TCheckBox *Conversion;
-	TComboBox *InFormat;
-	TComboBox *OutFormat;
-	TLabel *Label2;
-	TLabel *Label10;
-	TEdit *Options;
-	TLabel *Label4;
-	TEdit *OutMsgs;
-	void __fastcall BtnOkClick(TObject *Sender);
-	void __fastcall ConversionClick(TObject *Sender);
-	void __fastcall FormShow(TObject *Sender);
+    TButton *BtnOk;
+    TButton *BtnCancel;
+    TCheckBox *Conversion;
+    TComboBox *InFormat;
+    TComboBox *OutFormat;
+    TLabel *Label2;
+    TLabel *Label10;
+    TEdit *Options;
+    TLabel *Label4;
+    TEdit *OutMsgs;
+    void __fastcall BtnOkClick(TObject *Sender);
+    void __fastcall ConversionClick(TObject *Sender);
+    void __fastcall FormShow(TObject *Sender);
 private:
-	void __fastcall UpdateEnable(void);
+    void __fastcall UpdateEnable(void);
 public:
-	AnsiString ConvMsg,ConvOpt,AntType,RcvType;
-	int ConvEna,ConvInp,ConvOut,StaId;
-	double AntPos[3],AntOff[3];
-	
-	__fastcall TConvDialog(TComponent* Owner);
+    AnsiString ConvMsg,ConvOpt,AntType,RcvType;
+    int ConvEna,ConvInp,ConvOut,StaId;
+    double AntPos[3],AntOff[3];
+
+    __fastcall TConvDialog(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TConvDialog *ConvDialog;
