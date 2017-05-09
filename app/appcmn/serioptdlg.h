507b8274f12d767fe687b1245683aa0cbbd30ba3
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:43 -0300

    serioptdlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/appcmn/serioptdlg.h
+++ app/appcmn/serioptdlg.h
@@ -10,34 +10,34 @@
 class TSerialOptDialog : public TForm
 {
 __published:
-	TButton *BtnOk;
-	TButton *BtnCancel;
-	TComboBox *BitRate;
-	TLabel *Label1;
-	TLabel *Label3;
-	TComboBox *Port;
-	TLabel *Label2;
-	TComboBox *ByteSize;
-	TLabel *Label4;
-	TComboBox *Parity;
-	TLabel *Label5;
-	TComboBox *StopBits;
-	TLabel *Label8;
-	TComboBox *FlowCtr;
-	TButton *BtnCmd;
-	TCheckBox *OutTcpPort;
-	TEdit *TcpPort;
-	void __fastcall FormShow(TObject *Sender);
-	void __fastcall BtnOkClick(TObject *Sender);
-	void __fastcall BtnCmdClick(TObject *Sender);
-	void __fastcall OutTcpPortClick(TObject *Sender);
+    TButton *BtnOk;
+    TButton *BtnCancel;
+    TComboBox *BitRate;
+    TLabel *Label1;
+    TLabel *Label3;
+    TComboBox *Port;
+    TLabel *Label2;
+    TComboBox *ByteSize;
+    TLabel *Label4;
+    TComboBox *Parity;
+    TLabel *Label5;
+    TComboBox *StopBits;
+    TLabel *Label8;
+    TComboBox *FlowCtr;
+    TButton *BtnCmd;
+    TCheckBox *OutTcpPort;
+    TEdit *TcpPort;
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall BtnOkClick(TObject *Sender);
+    void __fastcall BtnCmdClick(TObject *Sender);
+    void __fastcall OutTcpPortClick(TObject *Sender);
 private:
-	void __fastcall UpdatePortList(void);
-	void __fastcall UpdateEnable(void);
+    void __fastcall UpdatePortList(void);
+    void __fastcall UpdateEnable(void);
 public:
-	AnsiString Path,Cmds[2];
-	int Opt,CmdEna[2];
-	__fastcall TSerialOptDialog(TComponent* Owner);
+    AnsiString Path,Cmds[2];
+    int Opt,CmdEna[2];
+    __fastcall TSerialOptDialog(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TSerialOptDialog *SerialOptDialog;
