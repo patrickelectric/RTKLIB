8a75138bc140c750b1f579f91b1e9cbedb03d804
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:45 -0300

    rcvoptdlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtknavi/rcvoptdlg.h
+++ app/rtknavi/rcvoptdlg.h
@@ -10,15 +10,15 @@
 class TRcvOptDialog : public TForm
 {
 __published:
-	TButton *BtnOk;
-	TButton *BnCancel;
-	TEdit *OptionE;
-	void __fastcall FormShow(TObject *Sender);
-	void __fastcall BtnOkClick(TObject *Sender);
+    TButton *BtnOk;
+    TButton *BnCancel;
+    TEdit *OptionE;
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall BtnOkClick(TObject *Sender);
 private:
 public:
-	AnsiString Option;
-	__fastcall TRcvOptDialog(TComponent* Owner);
+    AnsiString Option;
+    __fastcall TRcvOptDialog(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TRcvOptDialog *RcvOptDialog;
