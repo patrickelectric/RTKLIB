de9cfe1013153103cc5c952698fa5ca5aeb668f1
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:47 -0300

    convdlg.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/strsvr/convdlg.cpp
+++ app/strsvr/convdlg.cpp
@@ -11,52 +11,52 @@ TConvDialog *ConvDialog;
 //---------------------------------------------------------------------------
 static double str2dbl(AnsiString str)
 {
-	double val=0.0;
-	sscanf(str.c_str(),"%lf",&val);
-	return val;
+    double val=0.0;
+    sscanf(str.c_str(),"%lf",&val);
+    return val;
 }
 //---------------------------------------------------------------------------
 __fastcall TConvDialog::TConvDialog(TComponent* Owner)
-	: TForm(Owner)
+    : TForm(Owner)
 {
-	int i;
-	for (i=0;i<=MAXRCVFMT;i++) {
-		InFormat->Items->Add(formatstrs[i]);
-	}
-	InFormat->ItemIndex=0;
+    int i;
+    for(i=0; i<=MAXRCVFMT; i++) {
+        InFormat->Items->Add(formatstrs[i]);
+    }
+    InFormat->ItemIndex=0;
 }
 //---------------------------------------------------------------------------
 void __fastcall TConvDialog::FormShow(TObject *Sender)
 {
-	AnsiString s;
-	Conversion->Checked=ConvEna;
-	InFormat ->ItemIndex=ConvInp;
-	OutFormat->ItemIndex=ConvOut;
-	OutMsgs->Text=ConvMsg;
-	Options->Text=ConvOpt;
-	UpdateEnable();
+    AnsiString s;
+    Conversion->Checked=ConvEna;
+    InFormat ->ItemIndex=ConvInp;
+    OutFormat->ItemIndex=ConvOut;
+    OutMsgs->Text=ConvMsg;
+    Options->Text=ConvOpt;
+    UpdateEnable();
 }
 //---------------------------------------------------------------------------
 void __fastcall TConvDialog::BtnOkClick(TObject *Sender)
 {
-	ConvEna=Conversion->Checked;
-	ConvInp=InFormat->ItemIndex;
-	ConvOut=OutFormat->ItemIndex;
-	ConvMsg=OutMsgs->Text;
-	ConvOpt=Options->Text;
+    ConvEna=Conversion->Checked;
+    ConvInp=InFormat->ItemIndex;
+    ConvOut=OutFormat->ItemIndex;
+    ConvMsg=OutMsgs->Text;
+    ConvOpt=Options->Text;
 }
 //---------------------------------------------------------------------------
 void __fastcall TConvDialog::ConversionClick(TObject *Sender)
 {
-	UpdateEnable();
+    UpdateEnable();
 }
 //---------------------------------------------------------------------------
 void __fastcall TConvDialog::UpdateEnable(void)
 {
-	InFormat ->Enabled=Conversion->Checked;
-	OutFormat->Enabled=Conversion->Checked;
-	OutMsgs  ->Enabled=Conversion->Checked;
-	Options  ->Enabled=Conversion->Checked;
+    InFormat ->Enabled=Conversion->Checked;
+    OutFormat->Enabled=Conversion->Checked;
+    OutMsgs  ->Enabled=Conversion->Checked;
+    Options  ->Enabled=Conversion->Checked;
 }
 //---------------------------------------------------------------------------
 
