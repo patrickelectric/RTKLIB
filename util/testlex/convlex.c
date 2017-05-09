8263ca183a390046b054591eebc0fb72600624bb
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:49 -0300

    convlex.c: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- util/testlex/convlex.c
+++ util/testlex/convlex.c
@@ -12,18 +12,22 @@ int main(int argc, char **argv)
     const char *usage="lexconvbin [-h] [-t type] infile [-o outfile]";
     char *infile="",*outfile="";
     int i,type=0,format=0;
-    
-    for (i=0;i<argc;i++) {
-        if      (!strcmp(argv[i],"-h")) format=1;
-        else if (!strcmp(argv[i],"-t")&&i+1<argc) type=atoi(argv[++i]);
-        else if (!strcmp(argv[i],"-o")&&i+1<argc) outfile=argv[++i];
-        else if (!strcmp(argv[i],"-")) {
+
+    for(i=0; i<argc; i++) {
+        if(!strcmp(argv[i],"-h")) {
+            format=1;
+        } else if(!strcmp(argv[i],"-t")&&i+1<argc) {
+            type=atoi(argv[++i]);
+        } else if(!strcmp(argv[i],"-o")&&i+1<argc) {
+            outfile=argv[++i];
+        } else if(!strcmp(argv[i],"-")) {
             fprintf(stderr,"usage: %s\n",usage);
             return 0;
+        } else {
+            infile=argv[i];
         }
-        else infile=argv[i];
     }
     lexconvbin(type,format,infile,outfile);
-    
+
     return 0;
 }
