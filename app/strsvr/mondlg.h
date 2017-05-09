4c308717473dc6a0945433b88ac6d99d2a66fd56
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:47 -0300

    mondlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/strsvr/mondlg.h
+++ app/strsvr/mondlg.h
@@ -16,34 +16,34 @@
 class TStrMonDialog : public TForm
 {
 __published:
-	TPanel *Panel1;
-	TPanel *Panel2;
-	TButton *BtnClose;
-	TSpeedButton *BtnClear;
-	TPaintBox *Console;
-	TScrollBar *Scroll;
-	TSpeedButton *BtnDown;
-	TSpeedButton *BtnStop;
-	TComboBox *SelFmt;
-	void __fastcall BtnCloseClick(TObject *Sender);
-	void __fastcall BtnClearClick(TObject *Sender);
-	void __fastcall ConsolePaint(TObject *Sender);
-	void __fastcall ScrollChange(TObject *Sender);
-	void __fastcall FormResize(TObject *Sender);
-	void __fastcall BtnDownClick(TObject *Sender);
-	void __fastcall BtnStopClick(TObject *Sender);
-	void __fastcall SelFmtChange(TObject *Sender);
+    TPanel *Panel1;
+    TPanel *Panel2;
+    TButton *BtnClose;
+    TSpeedButton *BtnClear;
+    TPaintBox *Console;
+    TScrollBar *Scroll;
+    TSpeedButton *BtnDown;
+    TSpeedButton *BtnStop;
+    TComboBox *SelFmt;
+    void __fastcall BtnCloseClick(TObject *Sender);
+    void __fastcall BtnClearClick(TObject *Sender);
+    void __fastcall ConsolePaint(TObject *Sender);
+    void __fastcall ScrollChange(TObject *Sender);
+    void __fastcall FormResize(TObject *Sender);
+    void __fastcall BtnDownClick(TObject *Sender);
+    void __fastcall BtnStopClick(TObject *Sender);
+    void __fastcall SelFmtChange(TObject *Sender);
 private:
-	TStringList *ConBuff;
-	int Stop,ScrollPos;
-	rtcm_t rtcm;
-	raw_t raw;
-	
-	void __fastcall AddConsole(unsigned char *msg, int len, int mode);
+    TStringList *ConBuff;
+    int Stop,ScrollPos;
+    rtcm_t rtcm;
+    raw_t raw;
+
+    void __fastcall AddConsole(unsigned char *msg, int len, int mode);
 public:
-	int StrFmt;
-	__fastcall TStrMonDialog(TComponent* Owner);
-	void __fastcall AddMsg(unsigned char *buff, int n);
+    int StrFmt;
+    __fastcall TStrMonDialog(TComponent* Owner);
+    void __fastcall AddMsg(unsigned char *buff, int n);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TStrMonDialog *StrMonDialog;
