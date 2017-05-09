caf684a1142fae98bdc037fa05c62809bc36bbc4
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:44 -0300

    staoptdlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkget/staoptdlg.h
+++ app/rtkget/staoptdlg.h
@@ -12,21 +12,21 @@
 class TStaListDialog : public TForm
 {
 __published:
-	TPanel *Panel1;
-	TButton *BtnLoad;
-	TButton *BtnSave;
-	TButton *BtnOk;
-	TButton *BtnCancel;
-	TMemo *StaList;
-	TOpenDialog *OpenDialog;
-	TSaveDialog *SaveDialog;
-	void __fastcall BtnLoadClick(TObject *Sender);
-	void __fastcall BtnOkClick(TObject *Sender);
-	void __fastcall FormShow(TObject *Sender);
-	void __fastcall BtnSaveClick(TObject *Sender);
+    TPanel *Panel1;
+    TButton *BtnLoad;
+    TButton *BtnSave;
+    TButton *BtnOk;
+    TButton *BtnCancel;
+    TMemo *StaList;
+    TOpenDialog *OpenDialog;
+    TSaveDialog *SaveDialog;
+    void __fastcall BtnLoadClick(TObject *Sender);
+    void __fastcall BtnOkClick(TObject *Sender);
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall BtnSaveClick(TObject *Sender);
 private:
 public:
-	__fastcall TStaListDialog(TComponent* Owner);
+    __fastcall TStaListDialog(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TStaListDialog *StaListDialog;
