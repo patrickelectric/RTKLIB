27ee63f058c693052647809d436af7582631b114
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:43 -0300

    fileoptdlg.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/appcmn_qt/fileoptdlg.cpp
+++ app/appcmn_qt/fileoptdlg.cpp
@@ -17,7 +17,7 @@ FileOptDialog::FileOptDialog(QWidget* parent)
     : QDialog(parent)
 {
     setupUi(this);
-	Opt=0;
+    Opt=0;
 
     keyDialog=new KeyDialog(this);
 
@@ -38,7 +38,9 @@ FileOptDialog::FileOptDialog(QWidget* parent)
 //---------------------------------------------------------------------------
 void FileOptDialog::showEvent(QShowEvent *event)
 {
-    if (event->spontaneous()) return;
+    if(event->spontaneous()) {
+        return;
+    }
 
     ChkTimeTag->setText(Opt?tr("TimeTag"):tr("Time"));
     TimeSpeed->setVisible(!Opt);
@@ -52,93 +54,104 @@ void FileOptDialog::showEvent(QShowEvent *event)
     BtnKey   ->setVisible(Opt);
     ChkTimeTag->setChecked(false);
 
-    if (!Opt) {
+    if(!Opt) {
         double speed=1.0,start=0.0;
 
         QStringList tokens=Path.split("::");
 
         QString token;
-        foreach (token, tokens)
-        {
-            if (token=="T") ChkTimeTag->setChecked(true);
-            if (token.contains("+")) start=token.toDouble();
-            if (token.contains("x")) speed=token.mid(1).toDouble();
+        foreach(token, tokens) {
+            if(token=="T") {
+                ChkTimeTag->setChecked(true);
+            }
+            if(token.contains("+")) {
+                start=token.toDouble();
+            }
+            if(token.contains("x")) {
+                speed=token.mid(1).toDouble();
+            }
         }
 
-		if (start<=0.0) start=0.0;
-		if (speed<=0.0) speed=1.0;
+        if(start<=0.0) {
+            start=0.0;
+        }
+        if(speed<=0.0) {
+            speed=1.0;
+        }
 
         int index=TimeSpeed->findText(QString("x%1").arg(speed));
-        if (index!=-1)
+        if(index!=-1) {
             TimeSpeed->setCurrentIndex(index);
-        else {
+        } else {
             TimeSpeed->addItem(QString("x%1").arg(speed),speed);
             TimeSpeed->setCurrentIndex(TimeSpeed->count());
         }
         TimeStart->setText(QString::number(start));
 
         FilePath->setText(tokens.at(0));
-	}
-	else {
+    } else {
         double intv=0.0;
 
         QStringList tokens=Path.split("::");
 
         QString token;
-        foreach (token, tokens)
-        {
-            if (token=="T") ChkTimeTag->setChecked(true);
-            if (token.contains("S=")) intv=token.mid(2).toDouble();
+        foreach(token, tokens) {
+            if(token=="T") {
+                ChkTimeTag->setChecked(true);
+            }
+            if(token.contains("S=")) {
+                intv=token.mid(2).toDouble();
+            }
         };
         int index=SwapIntv->findText(QString::number(intv,'g',3));
-        if (index!=-1)
+        if(index!=-1) {
             SwapIntv->setCurrentIndex(index);
-        else {
+        } else {
             SwapIntv->addItem(QString::number(intv,'g',3),intv);
             SwapIntv->setCurrentIndex(TimeSpeed->count());
         }
 
         FilePath->setText(tokens.at(0));
-	}
-	UpdateEnable();
+    }
+    UpdateEnable();
 }
 //---------------------------------------------------------------------------
 void FileOptDialog::BtnOkClick()
 {
     QString str;
     bool okay;
-	
-	if (!Opt) {
+
+    if(!Opt) {
         Path=FilePath->text();
-        if (ChkTimeTag->isChecked()) {
+        if(ChkTimeTag->isChecked()) {
             Path=Path+"::T"+"::"+TimeSpeed->currentText()+"::+"+TimeStart->text();
-		}
-	}
-	else {
+        }
+    } else {
         Path=FilePath->text();
-        if (ChkTimeTag->isChecked()) Path+="::T";
+        if(ChkTimeTag->isChecked()) {
+            Path+="::T";
+        }
         str=SwapIntv->currentText();
         str.toDouble(&okay);
-        if (okay) {
-			Path+="::S="+str;
-		}
-	}
+        if(okay) {
+            Path+="::S="+str;
+        }
+    }
     accept();
 }
 //---------------------------------------------------------------------------
 void FileOptDialog::BtnFilePathClick()
 {
-	if (!Opt) {
+    if(!Opt) {
         FilePath->setText(QDir::toNativeSeparators(QFileDialog::getOpenFileName(this,QString(),FilePath->text())));
-	}
-	else {
+    } else {
         FilePath->setText(QDir::toNativeSeparators(QFileDialog::getSaveFileName(this,QString(),FilePath->text())));
     }
 }
 //---------------------------------------------------------------------------
 void FileOptDialog::ChkTimeTagClick()
 {
-	UpdateEnable();
+    UpdateEnable();
 }
 //---------------------------------------------------------------------------
 void FileOptDialog::BtnKeyClick()
