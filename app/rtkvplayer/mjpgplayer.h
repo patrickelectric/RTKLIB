c19768661b7bd91c0498888150d216d9c38ba664
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:47 -0300

    mjpgplayer.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkvplayer/mjpgplayer.h
+++ app/rtkvplayer/mjpgplayer.h
@@ -11,25 +11,25 @@
 class TMjpgPlayer
 {
 private:
-	unsigned char *LogData, *TagData;
-	size_t LogSize, TagSize;
-	int Width, Height;
-	int CurrentFrm, NumFrm;
-	double FPS;
-	int __fastcall OpenLog(UnicodeString file);
-	int __fastcall OpenTag(UnicodeString file);
+    unsigned char *LogData, *TagData;
+    size_t LogSize, TagSize;
+    int Width, Height;
+    int CurrentFrm, NumFrm;
+    double FPS;
+    int __fastcall OpenLog(UnicodeString file);
+    int __fastcall OpenTag(UnicodeString file);
 public:
-	int __fastcall OpenVideo(UnicodeString file);
-	void __fastcall ClearVideo(void);
-	int  __fastcall PlayVideo(void);
-	void __fastcall StopVideo(void);
-	void __fastcall SetVideoPos(float pos);
-	float __fastcall GetVideoPos(void);
-	void __fastcall GetVideoTime(double &time, double &period);
-	void __fastcall GetVideoSize(int &width, int &height);
-	void __fastcall UpdateVideo(void);
-	__fastcall TMjpgPlayer();
-	__fastcall ~TMjpgPlayer();
+    int __fastcall OpenVideo(UnicodeString file);
+    void __fastcall ClearVideo(void);
+    int  __fastcall PlayVideo(void);
+    void __fastcall StopVideo(void);
+    void __fastcall SetVideoPos(float pos);
+    float __fastcall GetVideoPos(void);
+    void __fastcall GetVideoTime(double &time, double &period);
+    void __fastcall GetVideoSize(int &width, int &height);
+    void __fastcall UpdateVideo(void);
+    __fastcall TMjpgPlayer();
+    __fastcall ~TMjpgPlayer();
 };
 
 #endif
