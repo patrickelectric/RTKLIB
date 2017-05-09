f899b80e91963181123e8c4ff2371d925c91b13a
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:46 -0300

    rtkplot.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkplot/rtkplot.cpp
+++ app/rtkplot/rtkplot.cpp
@@ -69,52 +69,44 @@ USEFORM("mapdlg.cpp", MapAreaDialog);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	try
-	{
-		Application->Initialize();
-		Application->Title = "RTKPLOT";
-		Application->CreateForm(__classid(TPlot), &Plot);
-		Application->CreateForm(__classid(TPlotOptDialog), &PlotOptDialog);
-		Application->CreateForm(__classid(TSatDialog), &SatDialog);
-		Application->CreateForm(__classid(TRefDialog), &RefDialog);
-		Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
-		Application->CreateForm(__classid(TSpanDialog), &SpanDialog);
-		Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
-		Application->CreateForm(__classid(TConnectDialog), &ConnectDialog);
-		Application->CreateForm(__classid(TSerialOptDialog), &SerialOptDialog);
-		Application->CreateForm(__classid(TTcpOptDialog), &TcpOptDialog);
-		Application->CreateForm(__classid(TCmdOptDialog), &CmdOptDialog);
-		Application->CreateForm(__classid(TFileOptDialog), &FileOptDialog);
-		Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
-		Application->CreateForm(__classid(TTextViewer), &TextViewer);
-		Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
-		Application->CreateForm(__classid(TPntDialog), &PntDialog);
-		Application->CreateForm(__classid(TConfDialog), &ConfDialog);
-		Application->CreateForm(__classid(TGoogleEarthView), &GoogleEarthView);
-		Application->CreateForm(__classid(TFtpOptDialog), &FtpOptDialog);
-		Application->CreateForm(__classid(TConsole), &Console);
-		Application->CreateForm(__classid(TGoogleMapView), &GoogleMapView);
-		Application->CreateForm(__classid(TMapAreaDialog), &MapAreaDialog);
-		Application->CreateForm(__classid(TSkyImgDialog), &SkyImgDialog);
-		Application->CreateForm(__classid(TVecMapDialog), &VecMapDialog);
-		Application->Run();
-	}
-	catch (Exception &exception)
-	{
-		Application->ShowException(&exception);
-	}
-	catch (...)
-	{
-		try
-		{
-			throw Exception("");
-		}
-		catch (Exception &exception)
-		{
-			Application->ShowException(&exception);
-		}
-	}
-	return 0;
+    try {
+        Application->Initialize();
+        Application->Title = "RTKPLOT";
+        Application->CreateForm(__classid(TPlot), &Plot);
+        Application->CreateForm(__classid(TPlotOptDialog), &PlotOptDialog);
+        Application->CreateForm(__classid(TSatDialog), &SatDialog);
+        Application->CreateForm(__classid(TRefDialog), &RefDialog);
+        Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
+        Application->CreateForm(__classid(TSpanDialog), &SpanDialog);
+        Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
+        Application->CreateForm(__classid(TConnectDialog), &ConnectDialog);
+        Application->CreateForm(__classid(TSerialOptDialog), &SerialOptDialog);
+        Application->CreateForm(__classid(TTcpOptDialog), &TcpOptDialog);
+        Application->CreateForm(__classid(TCmdOptDialog), &CmdOptDialog);
+        Application->CreateForm(__classid(TFileOptDialog), &FileOptDialog);
+        Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
+        Application->CreateForm(__classid(TTextViewer), &TextViewer);
+        Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
+        Application->CreateForm(__classid(TPntDialog), &PntDialog);
+        Application->CreateForm(__classid(TConfDialog), &ConfDialog);
+        Application->CreateForm(__classid(TGoogleEarthView), &GoogleEarthView);
+        Application->CreateForm(__classid(TFtpOptDialog), &FtpOptDialog);
+        Application->CreateForm(__classid(TConsole), &Console);
+        Application->CreateForm(__classid(TGoogleMapView), &GoogleMapView);
+        Application->CreateForm(__classid(TMapAreaDialog), &MapAreaDialog);
+        Application->CreateForm(__classid(TSkyImgDialog), &SkyImgDialog);
+        Application->CreateForm(__classid(TVecMapDialog), &VecMapDialog);
+        Application->Run();
+    } catch(Exception &exception) {
+        Application->ShowException(&exception);
+    } catch(...) {
+        try {
+            throw Exception("");
+        } catch(Exception &exception) {
+            Application->ShowException(&exception);
+        }
+    }
+    return 0;
 }
 //---------------------------------------------------------------------------
 
