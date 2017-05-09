6d15508b9590f0dbe79c0f9e21ebb42c908fe638
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:44 -0300

    launchoptdlg.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtklaunch/launchoptdlg.cpp
+++ app/rtklaunch/launchoptdlg.cpp
@@ -11,35 +11,31 @@
 TLaunchOptDialog *LaunchOptDialog;
 //---------------------------------------------------------------------------
 __fastcall TLaunchOptDialog::TLaunchOptDialog(TComponent* Owner)
-	: TForm(Owner)
+    : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
 void __fastcall TLaunchOptDialog::FormShow(TObject *Sender)
 {
-	if (MainForm->Option==1) {
-		OptMkl->Checked=true;
-	}
-	else if (MainForm->Option==2) {
-		OptWin64->Checked=true;
-	}
-	else {
-		OptNormal->Checked=true;
-	}
-	Minimize->Checked=MainForm->Minimize;
+    if(MainForm->Option==1) {
+        OptMkl->Checked=true;
+    } else if(MainForm->Option==2) {
+        OptWin64->Checked=true;
+    } else {
+        OptNormal->Checked=true;
+    }
+    Minimize->Checked=MainForm->Minimize;
 }
 //---------------------------------------------------------------------------
 void __fastcall TLaunchOptDialog::BtnOkClick(TObject *Sender)
 {
-	if (OptMkl->Checked) {
-		MainForm->Option=1;
-	}
-	else if (OptWin64->Checked) {
-		MainForm->Option=2;
-	}
-	else {
-		MainForm->Option=0;
-	}
-	MainForm->Minimize=Minimize->Checked;
+    if(OptMkl->Checked) {
+        MainForm->Option=1;
+    } else if(OptWin64->Checked) {
+        MainForm->Option=2;
+    } else {
+        MainForm->Option=0;
+    }
+    MainForm->Minimize=Minimize->Checked;
 }
 //---------------------------------------------------------------------------
