534611b39b8ef2b78079aea7c612706c8adf5897
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:43 -0300

    console.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/appcmn_qt/console.h
+++ app/appcmn_qt/console.h
@@ -11,22 +11,22 @@ class Console : public QDialog, private Ui::Console
     Q_OBJECT
 
 public slots:
-        void  BtnCloseClick();
-        void  BtnClearClick();
-        void  BtnAscClick();
-        void  BtnHexClick();
-        void  BtnDownClick();
+    void  BtnCloseClick();
+    void  BtnClearClick();
+    void  BtnAscClick();
+    void  BtnHexClick();
+    void  BtnDownClick();
 
 protected:
-        void  ScrollChange();
-        void  FormResize();
+    void  ScrollChange();
+    void  FormResize();
 
 private:
-        QStringList ConBuff;
+    QStringList ConBuff;
 
 public:
-        explicit Console(QWidget* parent);
-        void  AddMsg(unsigned char *msg, int n);
+    explicit Console(QWidget* parent);
+    void  AddMsg(unsigned char *msg, int n);
 };
 //---------------------------------------------------------------------------
 #endif
