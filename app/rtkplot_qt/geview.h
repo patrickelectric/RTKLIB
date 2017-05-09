f5c015b7f2da520faddc019a14f8cb21f6d0ad60
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:46 -0300

    geview.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkplot_qt/geview.h
+++ app/rtkplot_qt/geview.h
@@ -20,9 +20,15 @@ class GEPageState : public QObject
     Q_PROPERTY(QString text MEMBER text NOTIFY textChanged)
     Q_PROPERTY(QString view MEMBER view NOTIFY viewChanged)
 public:
-    explicit GEPageState(QObject *parent=NULL): QObject(parent){}
-    QString getText() {return text;}
-    QString getView() {return view;}
+    explicit GEPageState(QObject *parent=NULL): QObject(parent) {}
+    QString getText()
+    {
+        return text;
+    }
+    QString getView()
+    {
+        return view;
+    }
 signals:
     void textChanged(const QString &text);
     void viewChanged(const QString &view);
@@ -60,7 +66,7 @@ public slots:
     void PageLoaded(bool);
 
 protected:
-    void resizeEvent(QResizeEvent * );
+    void resizeEvent(QResizeEvent *);
 
 private:
     int State,Expand,Rotate,MarkVis[2],TrackVis[2];
@@ -75,32 +81,32 @@ private:
     QWebEngineView *WebBrowser;
     GEPageState *pageState;
 #endif
-    void UpdateOpts (void);
+    void UpdateOpts(void);
     void UpdateEnable(void);
-    void ExecFunc   (const QString &func);
+    void ExecFunc(const QString &func);
 
 public:
     explicit GoogleEarthView(QWidget *parent=NULL);
 
-    int  GetState   (void);
-    void Init       (void);
-    void Clear      (void);
-    void SetView    (double lat, double lon, double range, double heading);
-    void SetCent    (double lat, double lon);
-    void SetRange   (double range);
-    void SetHeading (double angle);
-    void SetMark    (int index, const double *pos);
-    void ShowMark   (int index);
-    void HideMark   (int index);
-    void ClearTrack (int index);
+    int  GetState(void);
+    void Init(void);
+    void Clear(void);
+    void SetView(double lat, double lon, double range, double heading);
+    void SetCent(double lat, double lon);
+    void SetRange(double range);
+    void SetHeading(double angle);
+    void SetMark(int index, const double *pos);
+    void ShowMark(int index);
+    void HideMark(int index);
+    void ClearTrack(int index);
     int  UpdateTrack(int index, solbuf_t *sol);
-    void ShowTrack  (int index);
-    void HideTrack  (int index);
+    void ShowTrack(int index);
+    void HideTrack(int index);
     void UpdatePoint(void);
-    void ShowPoint  (void);
-    void HidePoint  (void);
-    void SetOpts    (const int *opts);
-    void GetOpts    (int *opts);
+    void ShowPoint(void);
+    void HidePoint(void);
+    void SetOpts(const int *opts);
+    void GetOpts(int *opts);
 
     int FixCent;
 };
