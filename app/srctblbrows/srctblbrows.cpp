f4211ea47126b7eca5cc6af5477761401718f95f
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:47 -0300

    srctblbrows.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/srctblbrows/srctblbrows.cpp
+++ app/srctblbrows/srctblbrows.cpp
@@ -21,31 +21,23 @@ USEFORM("browsmain.cpp", MainForm);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	try
-	{
-		Application->Initialize();
-		Application->Title = "NTRIP Source Table Browser";
-		Application->CreateForm(__classid(TMainForm), &MainForm);
-		Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
-		Application->CreateForm(__classid(TGoogleMapView), &GoogleMapView);
-		Application->CreateForm(__classid(TStaListDialog), &StaListDialog);
-		Application->Run();
-	}
-	catch (Exception &exception)
-	{
-		Application->ShowException(&exception);
-	}
-	catch (...)
-	{
-		try
-		{
-			throw Exception("");
-		}
-		catch (Exception &exception)
-		{
-			Application->ShowException(&exception);
-		}
-	}
-	return 0;
+    try {
+        Application->Initialize();
+        Application->Title = "NTRIP Source Table Browser";
+        Application->CreateForm(__classid(TMainForm), &MainForm);
+        Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
+        Application->CreateForm(__classid(TGoogleMapView), &GoogleMapView);
+        Application->CreateForm(__classid(TStaListDialog), &StaListDialog);
+        Application->Run();
+    } catch(Exception &exception) {
+        Application->ShowException(&exception);
+    } catch(...) {
+        try {
+            throw Exception("");
+        } catch(Exception &exception) {
+            Application->ShowException(&exception);
+        }
+    }
+    return 0;
 }
 //---------------------------------------------------------------------------
