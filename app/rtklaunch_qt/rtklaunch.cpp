e28b5e01f5114ace2dae4740827b0aa49a231850
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:44 -0300

    rtklaunch.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtklaunch_qt/rtklaunch.cpp
+++ app/rtklaunch_qt/rtklaunch.cpp
@@ -21,29 +21,21 @@ USEFORM("launchmain.cpp", MainForm);
 //---------------------------------------------------------------------------
 WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
-	try
-	{
-		Application->Initialize();
-		Application->MainFormOnTaskBar = true;
-		Application->Title = "RTKLAUNCH";
-		Application->CreateForm(__classid(TMainForm), &MainForm);
-		Application->Run();
-	}
-	catch (Exception &exception)
-	{
-		Application->ShowException(&exception);
-	}
-	catch (...)
-	{
-		try
-		{
-			throw Exception("");
-		}
-		catch (Exception &exception)
-		{
-			Application->ShowException(&exception);
-		}
-	}
-	return 0;
+    try {
+        Application->Initialize();
+        Application->MainFormOnTaskBar = true;
+        Application->Title = "RTKLAUNCH";
+        Application->CreateForm(__classid(TMainForm), &MainForm);
+        Application->Run();
+    } catch(Exception &exception) {
+        Application->ShowException(&exception);
+    } catch(...) {
+        try {
+            throw Exception("");
+        } catch(Exception &exception) {
+            Application->ShowException(&exception);
+        }
+    }
+    return 0;
 }
 //---------------------------------------------------------------------------
