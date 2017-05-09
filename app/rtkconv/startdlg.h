8e471241ade879828c80ae2a160808a6725c45fc
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:44 -0300

    startdlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkconv/startdlg.h
+++ app/rtkconv/startdlg.h
@@ -13,27 +13,27 @@
 class TStartDialog : public TForm
 {
 __published:
-	TButton *BtnOk;
-	TButton *BtnCancel;
-	TPanel *Panel1;
-	TEdit *TimeY1;
-	TUpDown *TimeY1UD;
-	TEdit *TimeH1;
-	TUpDown *TimeH1UD;
-	TLabel *Label1;
-	TButton *BtnFileTime;
-	void __fastcall TimeY1UDChangingEx(TObject *Sender, bool &AllowChange,
-          short NewValue, TUpDownDirection Direction);
-	void __fastcall TimeH1UDChangingEx(TObject *Sender, bool &AllowChange,
-          short NewValue, TUpDownDirection Direction);
-	void __fastcall FormShow(TObject *Sender);
-	void __fastcall BtnOkClick(TObject *Sender);
-	void __fastcall BtnFileTimeClick(TObject *Sender);
+    TButton *BtnOk;
+    TButton *BtnCancel;
+    TPanel *Panel1;
+    TEdit *TimeY1;
+    TUpDown *TimeY1UD;
+    TEdit *TimeH1;
+    TUpDown *TimeH1UD;
+    TLabel *Label1;
+    TButton *BtnFileTime;
+    void __fastcall TimeY1UDChangingEx(TObject *Sender, bool &AllowChange,
+                                       short NewValue, TUpDownDirection Direction);
+    void __fastcall TimeH1UDChangingEx(TObject *Sender, bool &AllowChange,
+                                       short NewValue, TUpDownDirection Direction);
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall BtnOkClick(TObject *Sender);
+    void __fastcall BtnFileTimeClick(TObject *Sender);
 private:
 public:
-	const char *FileName;
-	gtime_t Time;
-	__fastcall TStartDialog(TComponent* Owner);
+    const char *FileName;
+    gtime_t Time;
+    __fastcall TStartDialog(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TStartDialog *StartDialog;
