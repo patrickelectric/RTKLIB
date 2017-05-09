a8d0d76a6a4db6f0ee81fe55a7ce36ac09f57f4e
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:44 -0300

    launchoptdlg.h: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtklaunch/launchoptdlg.h
+++ app/rtklaunch/launchoptdlg.h
@@ -11,17 +11,17 @@
 class TLaunchOptDialog : public TForm
 {
 __published:	// IDE で管理されるコンポーネント
-	TRadioButton *OptMkl;
-	TRadioButton *OptWin64;
-	TButton *BtnCancel;
-	TButton *BtnOk;
-	TRadioButton *OptNormal;
-	TCheckBox *Minimize;
-	void __fastcall FormShow(TObject *Sender);
-	void __fastcall BtnOkClick(TObject *Sender);
+    TRadioButton *OptMkl;
+    TRadioButton *OptWin64;
+    TButton *BtnCancel;
+    TButton *BtnOk;
+    TRadioButton *OptNormal;
+    TCheckBox *Minimize;
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall BtnOkClick(TObject *Sender);
 private:	// ユーザー宣言
 public:		// ユーザー宣言
-	__fastcall TLaunchOptDialog(TComponent* Owner);
+    __fastcall TLaunchOptDialog(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TLaunchOptDialog *LaunchOptDialog;
