5efbc8d3f0c0d6a18c392c157d4f91f8fe528e16
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-09 12:18:46 -0300

    rtkpost_win64.cpp: Correct code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkpost_win64/rtkpost_win64.cpp
+++ app/rtkpost_win64/rtkpost_win64.cpp
@@ -33,39 +33,31 @@ USEFORM("..\rtkpost\postmain.cpp", MainForm);
 //---------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	try
-	{
-		Application->Initialize();
-		Application->Title = "RTKPOST";
-		Application->CreateForm(__classid(TMainForm), &MainForm);
-		Application->CreateForm(__classid(TTextViewer), &TextViewer);
-		Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
-		Application->CreateForm(__classid(TRefDialog), &RefDialog);
-		Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
-		Application->CreateForm(__classid(TConfDialog), &ConfDialog);
-		Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
-		Application->CreateForm(__classid(TConvDialog), &ConvDialog);
-		Application->CreateForm(__classid(TOptDialog), &OptDialog);
-		Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
-		Application->CreateForm(__classid(TExtOptDialog), &ExtOptDialog);
-		Application->CreateForm(__classid(TMaskOptDialog), &MaskOptDialog);
-		Application->Run();
-	}
-	catch (Exception &exception)
-	{
-		Application->ShowException(&exception);
-	}
-	catch (...)
-	{
-		try
-		{
-			throw Exception("");
-		}
-		catch (Exception &exception)
-		{
-			Application->ShowException(&exception);
-		}
-	}
-	return 0;
+    try {
+        Application->Initialize();
+        Application->Title = "RTKPOST";
+        Application->CreateForm(__classid(TMainForm), &MainForm);
+        Application->CreateForm(__classid(TTextViewer), &TextViewer);
+        Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
+        Application->CreateForm(__classid(TRefDialog), &RefDialog);
+        Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
+        Application->CreateForm(__classid(TConfDialog), &ConfDialog);
+        Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
+        Application->CreateForm(__classid(TConvDialog), &ConvDialog);
+        Application->CreateForm(__classid(TOptDialog), &OptDialog);
+        Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
+        Application->CreateForm(__classid(TExtOptDialog), &ExtOptDialog);
+        Application->CreateForm(__classid(TMaskOptDialog), &MaskOptDialog);
+        Application->Run();
+    } catch(Exception &exception) {
+        Application->ShowException(&exception);
+    } catch(...) {
+        try {
+            throw Exception("");
+        } catch(Exception &exception) {
+            Application->ShowException(&exception);
+        }
+    }
+    return 0;
 }
 //---------------------------------------------------------------------------
